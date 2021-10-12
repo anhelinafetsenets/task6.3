8d2f82635b8904315434c717516e007f701135da
Ангеліна Феценець <Anhelina.Fetsenets.PZ.2020@edu.lpnu.ua>
2021-10-12 23:26:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace task6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack Matrix = new Stack();
+            for(int i = 0; i < 9; ++i)
+            {
+                Matrix.Push(i);
+            }
+            foreach(Object obj in Matrix)
+            {
+                Console.WriteLine($"{obj} ");
+            }
+        }
+    }
+}
